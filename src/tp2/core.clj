eeb7d445caea3ceec0899229e7e78768d1348170
Santy <shenseler@fi.uba.ar>
2024-05-19 13:13:11 -0300

    funcion extender expresiones

--- src/tp2/core.clj
+++ src/tp2/core.clj
@@ -1,22 +1,19 @@
 (ns tp2.core (:gen-class))
 (require '[tp2.sistemaL :as sistemaL])
-(require '[clojure.string :as str])
 
 
 (defn -main [& args]
-  ( if (= (count args) 3) ( do
-    ;; Parseo de argumentos
-    (def argumentos (vec args))
-    (def entrada (argumentos 0))
-    (def iteraciones (argumentos 1))
-    (def salida (argumentos 2))
-
-    ;; Parseo de archivo 
-    (def archivo ( sistemaL/openFile entrada ) )
-    (def angulo (archivo 0))
-    (def axioma (archivo 1))
-    (def reglas (subvec archivo 2)) ;; Vector con las lineas.
-    (println (str/split (reglas 0) #" ")) ;;  Separando por espacio 
-  )
+  ( if (= (count args) 3)
+   (let [
+        ;; Parseo de argumentos
+        argumentos (vec args)
+        entrada (argumentos 0)
+        iteraciones ( Integer/parseInt (argumentos 1))
+        salida (argumentos 2) 
+   ]
+      ;; Parseo de archivo   
+     (sistemaL/openFile entrada iteraciones)
+   )
+   
   ( println "Uso: lein run [archivo_entrada.sl] [cant_iteraciones] [archivo_salida.svg]") )
 )
--- src/tp2/sistemaL.clj
+++ src/tp2/sistemaL.clj
@@ -1,9 +1,50 @@
 (ns tp2.sistemaL (:gen-class))
 (use 'clojure.java.io)
+(require '[clojure.string :as str])
 
-(defn openFile 
-    [x]
-    (with-open [rdr (reader (str "doc/" x))]
-        (reduce conj [] (line-seq rdr)) ;; Crear lista de lineas
-    )    
+(defn make-vec
+  "Reemplaza el vector por las reglas"
+  [reglas vector]
+  (let [
+       nilVec '() 
+       newVec (for [x (range 0 (count vector))] 
+         (conj nilVec (reglas ((vec vector) x)))
+       )]
+    newVec
+  )
 )
+
+(defn iter
+  "Itera i veces"
+  [reglas i vector]
+  (println vector)
+  (if (> i 1)
+    (iter reglas (- i 1) (make-vec reglas vector))
+    vector
+    ) 
+)
+
+(defn reglas-dicc
+  "Transforma las reglas en un diccionario"
+  [reglas]
+  (let [
+       reglasVec (for [x (range 0 (count reglas))]
+                    (conj (str/split (reglas x) #" ")))
+       ]
+    (apply hash-map (flatten reglasVec))
+  ) 
+)
+
+(defn openFile
+  [file i]
+  (let [
+        archivo (with-open [rdr (reader (str "doc/" file))] 
+                  (reduce conj [] (line-seq rdr)) ;; Crear lista de lineas
+                  )
+        angulo (archivo 0)
+        axioma (archivo 1) 
+        reglas (subvec archivo 2);; Vector con las lineas.
+        ]
+    (println (iter (reglas-dicc reglas) i (list axioma)))
+    )
+  )
