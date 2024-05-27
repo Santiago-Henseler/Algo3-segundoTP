292216305784696c8a48dec8d215badeb7071099
sttesta3 <sttesta@fi.uba.ar>
2024-05-27 00:21:11 -0300

    WIP: Sigo debugeando, me voy a dormir

--- src/tp2/svgMaker.clj
+++ src/tp2/svgMaker.clj
@@ -23,29 +23,34 @@
   ( if (not (empty? formula)) (   
       if (= (first formula) "[") (    ;; Esto es un gran if else if else if else ...
         ;; Agregamos a la pila el mismo elemento que tenemos antes
-;;        ( println "DEBUG 2 LILT")
+        ( println "DEBUG 2 LILT")
         ( lineInLine_Testa (next formula) angulo (conj pila (peek pila)) )  
       ) ( 
       if (= (first formula) "]") (
-;;        ( println "DEBUG 3 LILT")
+        ( println "DEBUG 3 LILT")
         ( str "M " (nth (peek pila) 0) " " (nth (peek pila) 1) " " (lineInLine_Testa  (next formula) angulo (pop pila) ) ) 
         ;; Quitar tortuga y escribir linea con pluma levantada para volver a punto inicial 
       ) (
       if (= (first formula) "+") (
-;;        ( println "DEBUG 4 LILT")
+        ( println "DEBUG 4 LILT")
         ( lineInLine_Testa (next formula) angulo (conj (pop pila) '(vector '(nth (peek pila) 0) '(nth (peek pila) 1) '(+ (nth (peek pila) 2) angulo))) ) 
+        ;; Siguiente elemento en formula, quitamos elemento con angulo anterior y agregamos mismo elemento + angulo
       ) (
       if (= (first formula) "-") (
-;;        ( println "DEBUG 5 LILT")        
+        ( println "DEBUG 5 LILT")        
         ( lineInLine_Testa (next formula) angulo (conj (pop pila) '(vector '(nth (peek pila) 0) '(nth (peek pila) 1) '(- (nth (peek pila) 2) angulo))) )
+        ;; Siguiente elemento en formula, quitamos elemento con angulo anterior y agregamos mismo elemento - angulo
       ) (       ;; Else
-;;        ( println "DEBUG 6 LILT")    
-        ( str "L " (nth ( vector '(+ (nth (peek pila) 0) (* (math/cos angulo) 100)) '(+ (nth (peek pila) 1) (* (* (math/sin angulo) 100) -1)) ) 0) " " (nth ( vector '(+ (nth (peek pila) 0) (* (math/cos angulo) 100)) '(+ (nth (peek pila) 1) (* (* (math/sin angulo) 100) -1)) ) 1) " " (lineInLine_Testa  (next formula) angulo pila ) )   
-      )))) 
-    )
+        ( println "DEBUG 6 LILT")    
+;;        ( str "L " (nth ( vector '(+ (nth (peek pila) 0) (* (math/cos angulo) 100)) '(+ (nth (peek pila) 1) (* (* (math/sin angulo) 100) -1)) ) 0) " " (nth ( vector '(+ (nth (peek pila) 0) (* (math/cos angulo) 100)) '(+ (nth (peek pila) 1) (* (* (math/sin angulo) 100) -1)) ) 1) " " (lineInLine_Testa  (next formula) angulo pila ) )   
+        ( str "L " 
+          (+ (nth (peek pila) 0) (* (Math/cos (nth (peek pila) 2)) 100)) " " 
+          (+ (nth (peek pila) 1) (* (Math/sin (nth (peek pila) 2)) -100)) " " 
+          ( lineInLine_Testa  (next formula) angulo (conj (pop pila) '(vector '(+ (nth (peek pila) 0) (* (math/cos angulo) 100)) '(+ (nth (peek pila) 1) (* (math/sin angulo) -100)) '(nth (peek pila) 2)) ) ) " "  
+    )))))) 
     (
 ;;      ( println "DEBUG 7 LILT")    
-        ( str "" )
+         "" 
 ;;      ( str nil )
 ;;      ( println "DEBUG 7 LILT OUT")    
     )
@@ -56,7 +61,7 @@
   (println "DEBUG 0 writesvg")
   (spit salida "<svg viewBox=\"-50 -150 300 200\" xmlns=\"http://www.w3.org/2000/svg\">" :append true)
   (println "DEBUG 1 writesvg")
-  (if (string? ( lineInLine_Testa expresions angulo (list '(vector '0 '0 'angulo)) ) ) (println "Es string") (println "No es string"))
+  (if (string? ( lineInLine_Testa expresions angulo (list '(vector '0 '0 '0)) ) ) (println "Es string") (println "No es string"))
   (println "DEBUG 2 writesvg")
 ;;  (spit salida (str "<path d=\"M 0 0" (str (lineInLine_Testa expresions angulo (list '(vector '0 '0 'angulo))) (char 34)) " stroke-width=\"1\" stroke=\"black\" fill=\"none\"/>") :append true) 
   (println "DEBUG 3 writesvg")
