8848390885e05b3b79c2196b83aaf169dd9da54f
Santy <shenseler@fi.uba.ar>
2024-05-27 17:12:06 -0300

    implemento turtle

--- src/tp2/svgMaker.clj
+++ src/tp2/svgMaker.clj
@@ -1,25 +1,31 @@
 (ns tp2.svgMaker  (:gen-class))
 
-(defn a
-  [angulo x]
-  (if (= x "F")
-    " l 10 0"
-    (if (= x "+")
-      (str " l " angulo " " angulo) 
-      (if (= x "-") 
-        (str " l -" angulo " -" angulo)
-        ""
-        )
-      )
-    )
+(defn turtle
+  [expresions vector xTur angulo pos]
+  (if-not (empty? expresions)
+    (if (= (first expresions) "F")
+      (turtle (rest expresions) (conj vector (str " l 10 0")) xTur angulo (+ pos 10))
+      (if (= (first expresions) "+")
+        (turtle (rest expresions) (conj vector (str " l "angulo" " angulo)) xTur angulo pos) 
+        (if (= (first expresions) "-") 
+          (turtle (rest expresions) (conj vector (str " l -"angulo" -" angulo)) xTur angulo pos) 
+          (if (= (first expresions) "X")
+            (turtle (rest expresions) (conj vector (str " l 10 0")) xTur angulo (+ pos 10))
+            (if (= (first expresions) "[")
+              (turtle (rest expresions) vector (conj (vec xTur) pos) angulo pos)   
+              (turtle (rest expresions) (conj vector (str " M "(peek xTur)" " 0)) (vec (drop-last xTur)) angulo (peek xTur))
+            )))))
+  vector
   )
+)
+
 
 (defn lineInLine [expresions angulo] 
-   (map (partial a angulo) expresions)
+  ; (map (partial a angulo) expresions)
 )
  
 (defn writeSvg [expresions angulo] 
-  (spit "test.svg" "<svg viewBox=\"-50 -150 300 200\" xmlns=\"http://www.w3.org/2000/svg\">" :append true)
-  (spit "test.svg" (str "<path d=\"M 0 0" (apply str (lineInLine expresions angulo))  " stroke-width=\"1\" stroke=\"black\" fill=\"none\"/>") :append true) 
+  (spit "test.svg" "<svg viewBox=\"-50 -150 2000 1000\" xmlns=\"http://www.w3.org/2000/svg\">" :append true)
+  (spit "test.svg" (str "<path d=\"M 0 0"(apply str (turtle expresions [] [] (read-string angulo) 0))"\" stroke-width=\"1\" stroke=\"black\" fill=\"none\"/>") :append true) 
   (spit "test.svg" "</svg>" :append true)
 )
